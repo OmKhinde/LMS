504991a3be40698d36a836de223d1451a99d7ca2
OmKhinde <omkhinde52@gmail.com>
2025-10-08 19:46:21 +0530

    just started the backend part

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+import { clerkWebhooks } from './controllers/webhooks.js'
+
+//initialize express
+const app = express()
+
+ await connectDB();
+
+
+//routes
+app.get('/',(req,res)=>{
+    res.send("api is working")
+})
+app.post('/clerk',express.json(),clerkWebhooks);
+
+//
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT , ()=>{
+    console.log(`server is running on port no. ${PORT}`);
+})
+
